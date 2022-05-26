9b93140024e8fe0658ff50078c6ac16b3e769883
philask <phil@philmarie.net>
2022-05-26 14:55:46 +0100

    Update wordle stats iOS Shortcut.js

--- wordle stats iOS Shortcut.js
+++ wordle stats iOS Shortcut.js
@@ -1,4 +1,9 @@
-# https://blog.kchung.co/how-to-save-your-wordle-scores/amp/
+/*
+* Credit to Keving Chung for original version, this one works with NYT Wordle
+*
+* https://blog.kchung.co/how-to-save-your-wordle-scores/amp/
+*
+*/
 
 wordleState = JSON.stringify(localStorage.getItem('nyt-wordle-state'));
 wordleStats = JSON.stringify(localStorage.getItem('nyt-wordle-statistics'));
@@ -7,3 +12,9 @@ window.prompt("Copy state", `var data = ${wordleState}; localStorage.setItem('ny
 window.prompt("Copy stats", `var data = ${wordleStats}; localStorage.setItem('nyt-wordle-statistics', data);`);
 
 completion(true);
+
+/*
+* Copy each (state & stats) to notes or wherever then run the resulting javascript on your new device
+*
+* If you want to go iOS to iOS don't forget to add completion(true); 
+*/
